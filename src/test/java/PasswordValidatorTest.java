d14627e488368565ed380e441d9d298f8b3dbc3a
Stanislav Platov <pllattoff@gmail.com>
2026-07-07 18:38:28 +0200

    update tests for method hasMinLength(String password, int min)

--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class PasswordValidatorTest {
 
     @Test
-    void hasMinLength_shouldReturnFalse_whenGiven8() {
-        assertFalse(PasswordValidator.hasMinLength("Abc1def", 8));
+    void hasMinLength_shouldReturnFalse_whenGiven7CharactersAnd8() {
+        assertFalse(PasswordValidator.hasMinLength("1234567", 8));
     }
     @Test
-    void hasMinLength_shouldReturnTrue_whenGiven8() {
-        assertTrue(PasswordValidator.hasMinLength("Abc1defg", 8));
+    void hasMinLength_shouldReturnTrue_whenGiven8CharactersAnd8() {
+        assertTrue(PasswordValidator.hasMinLength("12345678", 8));
     }
     @Test
     void hasMinLength_shouldReturnFalse_whenGivenNull() {
